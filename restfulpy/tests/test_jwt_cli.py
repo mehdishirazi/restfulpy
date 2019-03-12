0bf4199d0c3139ab6d0986f3e41916dab2de7660
mohammad <mohammadsheikhian70@gmail.com>
2019-03-12 17:04:44 +0330

    Tested the jwt cli, #220

---
+++ restfulpy/tests/test_jwt_cli.py
@@ -0,0 +1,48 @@
+import json
+
+import pytest
+from bddcli import Given, given, when, stdout, stderr, Application
+from itsdangerous import TimedJSONWebSignatureSerializer
+from itsdangerous.exc import SignatureExpired
+from nanohttp import settings
+
+from restfulpy import Application as RestfulPyApplication
+
+
+foo = RestfulPyApplication(name='jwt')
+foo.__configuration__ = ''
+
+
+def foo_main():
+    return foo.cli_main()
+
+
+app = Application('foo', 'restfulpy.tests.test_jwt_cli:foo_main')
+
+
+def test_jwt():
+    foo.configure(force=True)
+    pirincipal = TimedJSONWebSignatureSerializer(
+        settings.jwt.secret,
+        algorithm_name=settings.jwt.algorithm
+    )
+
+    with Given(app, ['jwt', 'create']):
+        assert stderr == ''
+        token = f'{stdout}'[:-1]
+        assert pirincipal.loads(token) == {}
+
+        # Create a jwt token with a payload
+        payload = dict(a=1)
+        when(given + f'\'{json.dumps(payload)}\'')
+        assert stderr == ''
+        token = f'{stdout}'[:-1]
+        assert pirincipal.loads(token) == payload
+
+        # Create a expire token
+        when(given + '-e -1')
+        assert stderr == ''
+        token = f'{stdout}'[:-1]
+        with pytest.raises(SignatureExpired):
+            pirincipal.loads(token)
+
